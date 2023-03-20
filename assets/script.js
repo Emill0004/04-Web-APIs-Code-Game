ca20b3027f4454f4da37a14330fd5e6df69b897c
Emill0004 <e.s.miller13125@gmail.com>
2023-03-19 20:08:40 -0600

    added a variable that stores a number which im going to use to show which part the application is on.

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,14 @@ var startPageEl = document.getElementById("startpg");
 var counterEl =  document.getElementById("counter");
 var contentPageEl =  document.getElementById("contentpg");
 
-var h1El = ["Coding Quiz challenge", "Highscores"];
-
-var h2El = ["Commonly used data types DO NOT include:", 
-"The condition in an if / else statement is enclosed within _____.", "Arrays in JavaScript can be used to store _____.", 
+var questionText = ["Commonly used data types DO NOT include:", 
+"The condition in an if / else statement is enclosed within _____.", 
+"Arrays in JavaScript can be used to store _____.", 
 "String values must be enclosed within _____ when being assigned to variables.", 
-"A very useful tool used during development and debugging for printing content to the debugger is:", "All done!", "Highscores"];
+"A very useful tool used during development and debugging for printing content to the debugger is:", 
+"All done!"];
+
+var centralCounter;
 
 // make an array with each question and have it cycle through each object
 function setTime() {
@@ -27,4 +29,6 @@ stButtonEl.addEventListener("click", function(event) {
     startPageEl.classList.toggle("hide");
     contentPageEl.classList.toggle("hide");
     setTime();
+    centralCounter = -1;
+    centralCounter++;
 });
